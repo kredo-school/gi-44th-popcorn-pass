e70b40c26862b982249cd4d003b1781b1ac12d93
Kredo-Garry <kredoteacher.grodriguez@gmail.com>
2026-06-19 18:53:45 +0800

    the route file has been updated/changed by Garry

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Auth::routes();
 // ===========================
 // Admin Routes
 // ===========================
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 });
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
