bcd3b0cee83ebd0efdd064c0c9697ec6823bdd09
ryohey-star <fvkvsoccer@gmail.com>
2026-06-19 20:07:26 +0900

    resolve merge conflict in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-=======
     <title>{{ config('app.name', 'Popcorn Pass') }}</title>
 
     <!-- Fonts -->
@@ -33,90 +32,42 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm py-0">
             <div class="container-fluid bg-black">
+
+                <!-- Logo -->
                 <a class="navbar-brand m-0 p-0" href="{{ url('/') }}">
                     <img src="{{ asset('storage/images/logo.png') }}" alt="Logo" width="50" height="50">
-
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Popcorn Pass') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                    </ul>
+                <div class="container">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav flex-row mx-auto align-items-center">
-                        <!-- Authentication Links -->
-                        <li class="nav-item me-5">
-                            <a href="#" class="text-decoration-none text-white nav-font">Now playing</a>
-                        </li>
-                        <li class="nav-item me-5 ms-5">
-                            <a href="#" class="text-decoration-none text-white nav-font">Coming soon</a>
-                        </li>
-                        <li class="nav-item  ms-5 me-5">
-                            <a href="#" class="text-decoration-none text-white nav-font">Food & Drink</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
+                        <!-- Center Links -->
+                        <ul class="navbar-nav flex-row mx-auto align-items-center">
+                            <li class="nav-item me-5">
+                                <a href="#" class="text-decoration-none text-white nav-font">Now playing</a>
+                            </li>
+                            <li class="nav-item me-5 ms-5">
+                                <a href="#" class="text-decoration-none text-white nav-font">Coming soon</a>
+                            </li>
+                            <li class="nav-item ms-5 me-5">
+                                <a href="#" class="text-decoration-none text-white nav-font">Food & Drink</a>
+                            </li>
+                        </ul>
+
+                        <!-- My Page -->
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
                                 <a href="#" class="btn btn-color mypage-text pt-0 pb-0">
-                                    <img src="{{ asset('storage/images/mypage.png') }}" alt="mypage"  width="35" height="35"> My Page
+                                    <img src="{{ asset('storage/images/mypage.png') }}" alt="mypage"
+                                         width="35" height="35">
+                                    My Page
                                 </a>
                             </li>
-                    </ul>
-                            
+                        </ul>
 
-=======
-    <li class="nav-item">
-        <a class="nav-link" href="#">
-            <img src="{{ asset('images/logo.png') }}" alt="logo" width="30" height="30">
-        </a>
-    </li>
-</ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        
-                            {{-- Now playing --}}
-                            <li>
-                                
-                            </li>
-                            {{-- Coming soon --}}
-                            <li class="nav-item" title="">
-                                <p class="text-white"></p>
-                            </li>
-                            {{-- Food & Drink --}}
-                            <li class="nav-item" title="">
-                               
-                            </li>
-                            {{-- Information --}}
-                            <li class="nav-item" title="">
-                                
-                            </li>
-                            <li class="nav-item" title="">
-                               <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-
-                                    @can('admin')
-                                        {{-- dashboard --}}
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fa-regular fa-face-smile"></i>Dashboard
-                                        </a>
-                                    @endcan
-                                   
-                                </div>
-                            </li>
-
-                    </ul>
+                    </div>
                 </div>
+
             </div>
         </nav>
 
@@ -131,5 +82,4 @@
         </main>
     </div>
 </body>
-
 </html>
