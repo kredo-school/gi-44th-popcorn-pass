a18ea75e8f8ddaaec96a3ceb78045f54ee63c891
mireisaito <mirei.saito4@gmail.com>
2026-08-05 22:34:40 +0900

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -657,7 +657,6 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 @endsection
 
-{{-- Add this section at the end of resources/views/home.blade.php --}}
 
 {{-- Recommended for You Section --}}
 @auth
